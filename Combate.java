00ebcabb18e0ac4574b967de64caa0bef3b1aa90
Andriuslima <andriuslima@outlook.com.br>
2016-11-10 13:01:28 -0200

    Class 'Combate' created

---
+++ Combate.java
@@ -0,0 +1,10 @@
+public class Combate{
+	
+	
+	public static void luta(Carta c1, Carta c2){
+		long bonus = 1.;
+		if(c1. getPoder()==cartaPoder.Time && c2.getPoder()==cartaPoder.Power 			|| c1.getPoder()==);
+		c2.setVida(c2.getVida()-( c1.getForca()*bonus / (c2.getDefesa()/2) ));
+	}	
+	
+}
