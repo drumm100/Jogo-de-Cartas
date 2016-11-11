b16f5bed23ddc3861fb932c2a10261c2f9fb9613
bwide <bfwide07@gmail.com>
2016-11-10 22:45:34 -0200

    classe "cards", que vai guardar todas as cartas

--- Combat.java
+++ Combat.java
@@ -1,5 +1,4 @@
 public class Combat{	
-
 	public static void atk(Card c1, Card c2){
 		double bonus = 1.0;
 		if( c1.type()==Type.TIME && c2.type()==Type.POWER ||
@@ -8,7 +7,8 @@ public class Combat{
 		    c1.type()==Type.REALITY && c2.type()==Type.SPACE ||
 		    c1.type()==Type.SPACE && c2.type()==Type.SOUL ||
 		    c1.type()==Type.SOUL && c2.type()==Type.REALITY )
-		   		bonus = 1.25;
+			bonus = 1.25;
+		//cartas que ganham vantagem (bonus 1.25)
 		else if( c1.type()==Type.POWER && c2.type()==Type.TIME ||
 		   		 c1.type()==Type.MIND && c2.type()==Type.POWER ||
 			     c1.type()==Type.REALITY && c2.type()==Type.MIND ||
@@ -16,8 +16,8 @@ public class Combat{
 		    	 c1.type()==Type.SOUL && c2.type()==Type.SPACE ||
 		    	 c1.type()==Type.REALITY && c2.type()==Type.SOUL )
 		   		bonus = 0.75;
-
+		//cartas que ganham desvantagem (bonus 0.75)
 		c2.setHP( (int)( c2.getHP()-( c1.getAtk()*bonus - c2.getDef()/4 ) ));
+		//aqui precisamos notificar os observadores (a futura classe tabuleiro, provavelmente)
 	}	
-	
 }
