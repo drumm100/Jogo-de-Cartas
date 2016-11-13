ec6e63fb8a411a3e8621cc16aead2c647e9fe5dd
bwide <bfwide07@gmail.com>
2016-11-12 23:29:34 -0200

    turnos

--- Table.java
+++ Table.java
@@ -3,14 +3,15 @@ public class Table{
     private static Table instance = new Table();    
     private Player player1;
     private Player player2;
+    private int turn;
 
     private Table(){
         player1 = new Player("");
         player2 = new Player("");
+        turn = 1; //turno, para decidir qual jogador vai jogar
     }
 
     public static Table getInstance(){return instance;}
 
     public Player player(int player){ return ( player == 1 ? player1 : player2 );}
-    
 }
