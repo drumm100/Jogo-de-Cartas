bf8154fed9c209f554d22d0db404191fb391b7d9
bwide <bfwide07@gmail.com>
2016-11-15 17:40:08 -0200

    clicks da mão do jogador

--- sample/Controller.java
+++ sample/Controller.java
@@ -54,16 +54,27 @@ public class Controller {
     public void deckClicked_P1 (MouseEvent event) {/* Este método irá ser chamado quando o deck do player 1 for clicado */
         hand1_P1.setImage(new Image("cartas/heroi128.jpg")); // so um exemplo de como funciona
     }
-    public void deckClicked_P2(MouseEvent event) {/* Este método irá ser chamado quando o deck do player 2 for clicado */
+    public void deckClicked_P2(MouseEvent event) {/* Este método irá ser chamado quando o deck do player 2 for clicado */}
+
+    public void ClickedHand1_P1(MouseEvent event) {
+        Game.getInstance().getPlayer().hand().play(1, Game.getInstance().getEnemyPlayer());
+    }
+    public void ClickedHand2_P1(MouseEvent event) {
+        Game.getInstance().getPlayer().hand().play(2, Game.getInstance().getEnemyPlayer());
+    }
+    public void ClickedHand3_P1(MouseEvent event) {
+        Game.getInstance().getPlayer().hand().play(3, Game.getInstance().getEnemyPlayer());
     }
 
-    public void ClickedHand1_P1(MouseEvent event) {/* Quando clicarem na primeira carta da Hand do player 1*/}
-    public void ClickedHand2_P1(MouseEvent event) {/* Quando clicarem na segunda carta da Hand do player 1*/}
-    public void ClickedHand3_P1(MouseEvent event) {/* Quando clicarem na terceira carta da Hand do player 1*/}
-
-    public void ClickedHand1_P2(MouseEvent event) {/* Quando clicarem na primeira carta da Hand do player 2*/}
-    public void ClickedHand2_P2(MouseEvent event) {/* Quando clicarem na segunda carta da Hand do player 2*/}
-    public void ClickedHand3_P2(MouseEvent event) {/* Quando clicarem na terceira carta da Hand do player 2*/}
+    public void ClickedHand1_P2(MouseEvent event) {
+        Game.getInstance().getPlayer().hand().play(1, Game.getInstance().getEnemyPlayer());
+    }
+    public void ClickedHand2_P2(MouseEvent event) {
+        Game.getInstance().getPlayer().hand().play(2, Game.getInstance().getEnemyPlayer());
+    }
+    public void ClickedHand3_P2(MouseEvent event) {
+        Game.getInstance().getPlayer().hand().play(3, Game.getInstance().getEnemyPlayer());
+    }
 
     public void ClickedField1(MouseEvent event) {/*Quando o field for clicado, independente do player*/}
     public void ClickedField2(MouseEvent event) {/*Quando o field for clicado, independente do player*/}
